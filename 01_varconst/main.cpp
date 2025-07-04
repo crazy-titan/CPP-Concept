8c9501d449d5f65932507864e41fe2ecc886d50c
Abhinav <abhinavspl2003@gmail.com>
2025-07-04 09:08:46 +0530

    add variable and constant file

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello ESP" << endl;
+    return 0; /*
+    this is because c++ is the statically language.
+    in function i mentioned int so the return of this function must be integer.
+    i can return any integer of my choice.
+    */
+}
+
+/* 
+Now we come to know about the data in C++ 
+there are two branch of data 
+store and process
+
+store means in form of int , float , string etc and process means loops , for , if else,
+for etc.
+
+so, these two are the backbone of C++ just learn these two and start making logics.
+
+And after store and process the last stage is display.
+*/
+
+/* identifires
+
+basically get to know about the types of variables, its just inbuilt of the language, 
+ie, i can give the name something like int int = 10, this will give errors.
+
+try to give the meaningful names
+*/
