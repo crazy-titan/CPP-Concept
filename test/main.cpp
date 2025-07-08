2f8b07e3be0b3c9764af63102818e01c5cee2e33
Abhinav <abhinavspl2003@gmail.com>
2025-07-08 08:43:49 +0530

    Understand syntax in C++

---
+++ test/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //# is known as preprocessor directives.
+
+using namespace std; /* this is a space provide to do you work inside this,
+you can also make you own*/
+int main(){
+    cout << "hello Abhinav";
+}
+/*This is a basic way to write the namespace ,you can also use 
+directly before and after the cout respectively in above syntax without 
+mentioning the using namespace std.
+
+std::cout << hello Abhinav << std::endl
+
+OR
+
+we can also write somethiing like 
+using std::cout
+using std::endl 
+on top of the main function
+*/
